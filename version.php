40c2fe6d8d60cfd78b01cf1814a30d270e033d7b
Jhon Rangel <jrangelardila@gmail.com>
2024-11-05 22:28:44 -0500

    Ajuste de acuerdo a lo reportado en 'Erro on edit couse topic label on last version!! #48'. @tpramos, dio el aporte

--- version.php
+++ version.php
@@ -26,6 +26,6 @@ defined('MOODLE_INTERNAL') || die();
 
 $plugin->component = 'format_buttons';
 $plugin->release = '4.x';
-$plugin->version = 2024110200;
+$plugin->version = 2024110500;
 $plugin->requires = 2023042400;
 $plugin->maturity = MATURITY_STABLE;
