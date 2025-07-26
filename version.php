79050c879a4ac599418966338c76508d4047bb6c
jrangelardila <jrangelardila@gmail.com>
2025-07-26 10:53:33 -0500

    Updated requires for issues:"Error with Moodle 4.5 - 5.x plugin (2025070400) #69" and "2025070401 throws error #68"

--- version.php
+++ version.php
@@ -26,6 +26,6 @@ defined('MOODLE_INTERNAL') || die();
 
 $plugin->component = 'format_buttons';
 $plugin->release = '4.x';
-$plugin->version = 2025040100;
+$plugin->version = 2025072600;
 $plugin->requires = 2023042400;
 $plugin->maturity = MATURITY_STABLE;
